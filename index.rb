b7b2c0e176f9aa2a48b7695a80c01425df8aaef0
mjamesv19 <mjamesv19@gmail.com>
2020-12-20 15:29:14 +1000

    finished goal and expensemethods

--- index.rb
+++ index.rb
@@ -42,8 +42,8 @@ def view_account(array, option)#local var for method
     list_of_account_names = []
     list_of_account_names_hash = {}
     array.each do |account| 
-        list_of_account_names << account.name # used in another method
-        list_of_account_names_hash[account.name] = account.funds
+        list_of_account_names << account.name # used in another method to view just names
+        list_of_account_names_hash[account.name] = account.funds #money and names
     end
     if option == "array"
         return list_of_account_names
@@ -95,32 +95,104 @@ def remove_income_from_list(array)
 end
 def add_income_on_list_to_accounts(array)
     $prompt.ask("adding income to accounts, press enter to continue")
-    money_to_be_added_array = []
     array.each do |account|
         account.income_hash.each_value do |value|
             account.funds += value 
         end
     end
-
+    puts view_account(array, "hash")
 end
 
-# def # add to income hash
-
-# end
-
-# testing
-# puts account_array
-# savings2.add_income(200)
-# puts savings2
-# income_hash = {}
-
-
-# print account_array
-# puts ""
-# puts "________"
-
-# @income_hash["testname"] = 50
-# puts @income_hash
+#expences menu
+def view_expenses_list(array)
+    array.each  do |account|
+        puts "    #{account.name} spends money on #{account.expense_hash}."
+    end
+end
+def create_new_expense(array)# select account 
+    response = $prompt.select("which account would you like to attach an expense to?", view_account(array, "array"))
+    array.each do |account|
+        if account.name == response
+            source_name = $prompt.ask("What is the name of the expense? ")
+            amount = $prompt.ask("How much money is did you spend? (must enter whole number, doesn't accept sense): ")
+            #checking if input is valid
+            if amount.to_i.to_s == amount
+                amount = amount.to_i
+                account.expense_hash[source_name] = amount
+                puts "    $#{amount}, spent #{source_name}, has been added to the expense list for #{account.name} account."
+            else
+                puts "You have provided invalid answer please try again and follow the prompts. "
+            end
+        end
+    end
+end
+def remove_expense_from_list(array)
+    response = $prompt.select("which account would you like to remove an expense from? ",view_account(array, "array"))
+    array.each do |account|
+        if account.name == response
+            if account.expense_hash != {} #this loop prevents an empty hash from being selected to avoid an error
+                keys_array = account.expense_hash.keys
+                # puts keys_array
+                source_name = $prompt.select("which source do you want to delete?", keys_array)
+                puts "    #{source_name} is being deleted from list"
+                account.expense_hash.delete(source_name)
+                puts 
+            else
+                puts "    ***This account has no expense to remove***"
+            end
+            
+        end
+    end 
+end
+def add_expense_on_list_to_accounts(array)
+    $prompt.ask("deducting expenses from accounts, press enter to continue")
+    array.each do |account|
+        account.expense_hash.each_value do |value|
+            account.funds -= value 
+        end
+    end
+    puts view_account(array, "hash")
+end
+# goal
+def how_long_to_goal(array)
+    response = $prompt.select("which account would you like calculate a goal?", view_account(array, "array"))
+    array.each do |account|
+        if account.name = response
+            goal_amount = $prompt.ask("this account has #{account.funds}, how much money do you want this account to reach? please enter whole number. ")
+            puts "taking the income and expense attached to this account"
+            sleep(1)
+            weeks = 0
+            if goal_amount.to_i.to_s != goal_amount
+                puts " you didn't enter a valid response please try again"
+                break
+            end
+            in_debt = "no"
+            start = account.funds
+            while start <= goal_amount.to_i 
+                account.income_hash.each_value do |value|
+                    start += value 
+                end
+                account.expense_hash.each_value do |value|
+                    start -= value 
+                end
+                if start <= 0
+                    in_debt = "yes"
+                    break
+                else
+                    weeks +=1
+                end
+            end
+            if in_debt == "yes"
+                puts "your expenses are greater than your income, can't caclulate time to goal. "
+            else
+                puts "starting with $#{account.funds}, it will take #{weeks} weeks to reach your goal of #{goal_amount}. "
+            end
+            break
+        else 
+            puts " you didn't enter a valid response please try again"
+        end
+    end
+end
 
 answer = ""
 while answer != "Exit"
@@ -146,7 +218,7 @@ while answer != "Exit"
             answer2 = income_menu
             case answer2
             when "View income sources"
-                view_income_list(account_array)
+               view_income_list(account_array)
                 
             when "Add new income source"
                 create_new_income(account_array)
@@ -165,16 +237,20 @@ while answer != "Exit"
             answer2 = expenses_menu
             case answer2
             when "View expenses list"
+                view_expenses_list(account_array)
             when "Add new expense"
+                create_new_expense(account_array)
             when "Remove expense from list"
+                remove_expense_from_list(account_array)
             when "Deduct current expenses from to connected accounts"
+                add_expense_on_list_to_accounts(account_array)
             when "Go Back" 
             end     
         end
     when "Goals"
-        puts "show goals"
+        how_long_to_goal(account_array)
     when "Transfer"
-        puts "5"
+        puts "feature coming soon"
     when "Exit"
     end
     
